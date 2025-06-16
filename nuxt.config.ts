3b36bd876de08ab19ac7e6cb8f02bccef06c3f40
kevocde <kevocde@gmail.com>
2025-06-16 18:24:42 -0500

    added: scroll behavior

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,16 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss'],
-  ssr: false
+  ssr: false,
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'icon',
+          type: 'image/png',
+          href: '/favicon.png'
+        }
+      ]
+    }
+  }
 })
